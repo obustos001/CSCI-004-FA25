e3d179e1373576a7be40a1980602d7d2ddc53087
you_love <obustos001@student.butte.edu>
2025-09-16 17:21:56 +0000

    hi

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,14 @@
+print("hello world")
+print("Chico Chai Spicy-Local-Fresh")
+print("Hello Paxton")
 
+evan = "evan is guapo"
+print(evan)
+print("evan", evan)
+hello = "world"
+print("hello", hello)
+
+this_number = "42"
+this_number =int(this_number) + 5
+
+this_number + 1
