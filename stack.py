c4f6e97893df8bf59346754a0bdb838a1dc05807
you_love <obustos001@student.butte.edu>
2025-11-13 20:04:25 +0000

    stack homework

--- stack.py
+++ stack.py
@@ -1,7 +1,13 @@
 stack = []
 
-def.stack 
+def push(stack, element):
+    stack.append(element)
+    return stack
 
-stack.pop()
+def pop(stack):
+    if len(stack) == 0:
+        return "Stack is empty"
+    return stack.pop()
 
-print (stack)
+def size(stack):
+    return len(stack)
